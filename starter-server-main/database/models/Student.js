2f41a1868538766e915619a5134fca1057d18da4
BDDoylan <bddoylan@gmail.com>
2021-07-06 11:12:12 -0400

    Implementing better models for campus and student.

--- starter-server-main/database/models/Student.js
+++ starter-server-main/database/models/Student.js
@@ -1,18 +1,34 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
 const Student = db.define("student", {
-
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    validate: { allowNull: false, notEmpty: true },
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
+    validate: { allowNull: false, notEmpty: true },
+  },
 
+  email: {
+    type: Sequelize.STRING,
+    validate: { allowNull: false, notEmpty: true, isEmail: true },
+  },
+
+  imageurl: {
+    type: Sequelize.STRING,
+    validate: {
+      isUrl: true,
+      defaultValue: "https://img.icons8.com/material-rounded/48/000000/nothing-found.png",
+    },
+  },
+
+  gpa: {
+    type: Sequelize.STRING,
+    validate: { isDecimal: true, min: 0.0, max: 4.0 },
+  },
 });
 
 module.exports = Student;
